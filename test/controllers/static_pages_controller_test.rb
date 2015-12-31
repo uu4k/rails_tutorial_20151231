53b1c7422c482e789d32ea007d6d6832c9da4ade
uu4k <t.shimizu1115@gmail.com>
2015-12-31 16:01:30 +0900

    レイアウトの変更にあわせてテストコード修正

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,19 +2,15 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-  def setup
-    @base_title = "Ruby on Rails Tutorial Sample App"
-  end
-
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 end
