1add8f1756f65859c8c7f639dffef560050c5680
uu4k <t.shimizu1115@gmail.com>
2015-12-31 21:39:50 +0900

    ログイン機能実装 ログイン状態テスト実装 テストユーザ用fixture設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,11 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
+
+  # 与えられた文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,4 +10,9 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+
+  # テストユーザーがログインしていればtrueを返す
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
